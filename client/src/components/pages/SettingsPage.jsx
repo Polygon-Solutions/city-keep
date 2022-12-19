6453b802cf892e44fb3ba561dca1cb79c180334a
Malaki Vandas <malakivandas@gmail.com>
2022-12-19 20:34:22 +0100

    Change password alerts

--- client/src/components/pages/SettingsPage.jsx
+++ client/src/components/pages/SettingsPage.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useContext } from 'react';
 
 import AccountContext from '../../context/account/AccountContext';
+import AlertsContext from '../../context/alerts/AlertsContext';
 
 import Page from '../layout/Page';
 import PageHeading from '../layout/PageHeading';
@@ -14,15 +15,18 @@ const SettingsPage = () => {
   const [newPassword, setNewPassword] = useState('');
 
   const { changePassword, logout } = useContext(AccountContext);
+  const { setAlert } = useContext(AlertsContext);
 
   const handleChangePassword = async (event) => {
     event.preventDefault();
+
     try {
       await changePassword(currentPassword, newPassword);
+      setAlert('Password changed successfully.', 'success');
       setCurrentPassword('');
       setNewPassword('');
     } catch (err) {
-      console.log(err);
+      setAlert(err.message, 'error');
     }
   };
 

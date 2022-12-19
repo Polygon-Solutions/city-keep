f592cfe9eb85a31ccd065d504a10b5edfcae718f
Malaki Vandas <malakivandas@gmail.com>
2022-12-19 12:02:22 +0100

    Alerts Context boilerplate

---
+++ client/src/context/alerts/AlertsState.js
@@ -0,0 +1,45 @@
+import React, { useReducer } from 'react';
+
+import AlertsContext from './AlertsContext';
+import AlertsReducer from './AlertsReducer';
+
+import { SET_ALERT, REMOVE_ALERT } from '../types';
+
+const AlertsState = ({ children }) => {
+  const initialState = [];
+
+  const [state, dispatch] = useReducer(
+    AlertsReducer,
+    initialState
+  );
+
+  const setAlert = (msg, type, timeout = 5000) => {
+    const id = uuidv4();
+    dispatch({
+      type: SET_ALERT,
+      payload: { msg, type, id },
+    });
+
+    setTimeout(
+      () =>
+        dispatch({
+          type: REMOVE_ALERT,
+          payload: id,
+        }),
+      timeout
+    );
+  };
+
+  return (
+    <AlertsContext.Provider
+      value={{
+        alerts: state,
+        setAlert,
+      }}
+    >
+      {children}
+    </AlertsContext.Provider>
+  );
+};
+
+export default AlertsState;

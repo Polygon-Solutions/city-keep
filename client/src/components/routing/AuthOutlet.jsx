fa885072adda4d315907129f761db0f5d3fe98b8
Malaki Vandas <malakivandas@gmail.com>
2023-05-16 11:21:20 +0200

    Route components

--- client/src/components/routing/AuthOutlet.jsx
+++ client/src/components/routing/AuthOutlet.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Navigate, Outlet } from 'react-router-dom';
 
+/**
+ * *
+ * AuthOutlet Component
+ * @description
+    - Renders the child routes if the user is logged in
+    - Navigates to the home page if the user is not logged in
+ * @prop {boolean} isAuthenticated - authentication status
+ * @listens Route (but imported into App.jsx)
+ */
 export const AuthOutlet = ({ isAuthenticated }) => {
+  // Render
   return isAuthenticated ? <Outlet /> : <Navigate to="/" />;
+};
+
+AuthOutlet.propTypes = {
+  isAuthenticated: PropTypes.bool.isRequired,
 };
 
 export default AuthOutlet;
--- client/src/components/routing/NoAuthOutlet.jsx
+++ client/src/components/routing/NoAuthOutlet.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Navigate, Outlet } from 'react-router-dom';
 
+/**
+ * *
+ * NoAuthOutlet Component
+ * @description
+    - Renders the child routes if the user is not logged in
+    - Navigates to the reports page if the user is logged in
+ * @prop {boolean} isAuthenticated - authentication status
+ * @listens Route (but imported into App.jsx)
+ */
 export const NoAuthOutlet = ({ isAuthenticated }) => {
+  // Render
   return !isAuthenticated ? (
     <Outlet />
   ) : (
     <Navigate to="/reports" />
   );
+};
+
+NoAuthOutlet.propTypes = {
+  isAuthenticated: PropTypes.bool.isRequired,
 };
 
 export default NoAuthOutlet;
